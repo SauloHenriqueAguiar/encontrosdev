53514b7681c532bf0504d629b3c1e5e85426a126
Saulo Henrique <saulo.aguiar@lbca.com.br>
2025-10-04 16:21:42 -0300

    32

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ main_logger = setup_logging(
     use_colors=use_colors
 )
 
+
 # Cria as tabelas no banco de dados
 main_logger.info("Criando tabelas no banco de dados")
 event_model.Base.metadata.create_all(bind=engine)
